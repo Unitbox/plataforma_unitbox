73ccf036afa28895d6ab56f0750b91c7a66a1277
Leonardo <leonardo.silva@sconline.com.br>
2021-12-15 14:02:18 -0300

    implementação email formulario

--- app/Http/Controllers/Web/FaleConoscoController.php
+++ app/Http/Controllers/Web/FaleConoscoController.php
@@ -23,21 +23,14 @@ class FaleConoscoController extends Controller
             'aceite' => 'required|max:191',
         ]);
         
-
         FaleConosco::create($request->all());
 
         $contato = new stdClass;
-        $contato->name = "Leonardo Augusto Soares da Silva";
-        $contato->to = ['leoaugusto45@gmail.com', 'leonardo.augusto@scservicos.com.br'];
         $contato->url = "https://uniplay.com.br";
 
-        // Mail::send('mail.web.faleconoscoemail', ['nome' => 'leonardo'], function($message) {
-        //     $message->from("contato@unitbox.com.br", 'Unitbox.com.br');
-        //     $message->subject('teste');
-        //     $message->to('leoaugusto45@gmail.com');
-        // });
-
-        Mail::send(new FaleConoscoMail($contato));
+        Mail::to('leoaugusto45@gmail.com')
+        //->cc($copia)
+        ->send(new FaleConoscoMail($contato));
 
         return response()->json([
             'status' => 'success', 
@@ -47,36 +40,7 @@ class FaleConoscoController extends Controller
 
     public function index() 
     {
-        $contato = new stdClass;
-        $contato->cc = ['leoaugusto45@gmail.com', 'leonardo.augusto@scservicos.com.br'];
-        $contato->url = "https://uniplay.com.br";
-
-        $copia =  ["leoaugusto45@gmail.com", "leonardo.silva@sconline.com.br"];
-
-        Mail::to(
-            'leonardo.augusto@scservicos.com.br'
-         )
-        ->cc($copia)
-        ->send(new FaleConoscoMail($contato));
-
-        // Mail::send('mail.web.faleconoscoemail', ['nome' => 'leonardo'], function($message) use ($teste) {
-        //     $message->from("contato@unitbox.com.br", 'Unitbox.com.br');
-        //     $message->subject('teste');
-        //     $message
-        //     $message->
-        // });
-
-
-        // Mail::to('leoaugusto45@gmail.com')
-        // //->cc($ccRecipients)
-        // ->send(new FaleConoscoMail($contato));
-
-
-        //Mail::send(new FaleConoscoMail($contato));
-
-        dd("mensagem Enviada"); 
-
-        //return new FaleConoscoMail($contato);
+        
 
     }
 }
