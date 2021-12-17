a1f7b9852a3b3769f9eb768b44727c49148680fe
Leonardo <leonardo.silva@sconline.com.br>
2021-12-17 00:21:16 -0300

    implementação cor de fundo twitter

--- resources/views/web/modules/header.blade.php
+++ resources/views/web/modules/header.blade.php
@@ -1,4 +1,4 @@
-<header id="header" class="navbar navbar-expand-lg navbar-end navbar-absolute-top navbar-dark navbar-show-hide"
+<header id="header" class="navbar navbar-expand-lg navbar-end navbar-absolute-top navbar-dark navbar-show-hide bg-dark-theme"
     data-hs-header-options='{
   "fixMoment": 1000,
   "fixEffect": "slide"
