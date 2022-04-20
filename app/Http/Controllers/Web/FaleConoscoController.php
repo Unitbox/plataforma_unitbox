5f32ec6a92662b312dbbdece73b9c6ba67d093c3
Leonardo <leoaugusto45@gmail.com>
2022-04-20 19:22:15 -0300

    testes

--- app/Http/Controllers/Web/FaleConoscoController.php
+++ app/Http/Controllers/Web/FaleConoscoController.php
@@ -7,36 +7,51 @@ use App\Mail\Web\FaleConosco as WebFaleConosco;
 use App\Mail\Web\FaleConoscoMail;
 use App\Models\Web\FaleConosco;
 use App\Models\Web\Interacao;
+use Curl\Curl;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 use stdClass;
 
 class FaleConoscoController extends Controller
 {
-    public function store(Request $request)
+    public function store()
     {  
-        $request->validate([
-            'name' => 'required|max:191',
-            'email' => 'email|required|max:191',
-            'company_name' => 'nullable|max:191',
-            'subject' => 'required|max:191',
-            'message' => 'required',
-            'aceite' => 'required|max:191',
-        ]);
-        
-        Interacao::create($request->all());
+        $contador = 1000;
+        $access_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvaW50ZWdyYS5zY3NlcnZpY29zLmFwcFwvYXBpXC9hdXRoXC9sb2dpbiIsImlhdCI6MTY1MDQ4MjY0MiwiZXhwIjoxMDE2MjgxMTMxNjIsIm5iZiI6MTY1MDQ4MjY0MiwianRpIjoiUWtrN05qa3lyOHVNaFpwaSIsInN1YiI6MSwicHJ2IjoiODdlMGFmMWVmOWZkMTU4MTJmZGVjOTcxNTNhMTRlMGIwNDc1NDZhYSJ9.waGbwS6vXSBL1AvNvbVC799dH1WAZNueXsbOvFyQ_Sw"; 
+        $response = "";
 
-        $contato = new stdClass;
-        $contato->url = "https://uniplay.com.br";
+        while ($contador >= 0) {
 
-        Mail::to('leoaugusto45@gmail.com')
-        //->cc($copia)
-        ->send(new FaleConoscoMail($contato));
+            $curl = new Curl();
+            $curl->setHeader('Authorization', 'Bearer ' . $access_token);
+            $curl->setHeader('tenant_id', '3810cb19-4033-44b6-bbdb-7c37e07403b8');
+            $curl->post('https://integra.scservicos.app/api/v1/fila/interacao', [
+                "remetente"=> "5511962690000",
+                "destinatario"=> "+5511982226852",
+                "template_name_id"=> "teste",
+                "campanha_id"=> 1,
+                "canal_id"=> 1,
+                "params" => [
+                    "var_1" => "Leonardo da Silva"
+                ]
+            ]);
 
-        return response()->json([
-            'status' => 'success', 
-            'subject' => 'Mensagem enviada com sucesso!', 
-            'message' => 'Em breve entraremo em contato. Obrigado!'], 200);
+            if ($curl->error) {
+                // $response->status = "danger";
+                // $response->message = (string)$curl->errorMessage;
+                // $response->data = $curl->response;
+
+                dd($curl->errorMessage, $curl->errorCode, $curl->response);
+
+            } else {
+                Log::info($contador);
+            }
+
+            $contador --;
+        }
+
+        dd("saiu");
     }
 
 }
--- resources/views/poc/medium.blade.php
+++ resources/views/poc/medium.blade.php
@@ -8,6 +8,7 @@
     <title>Medium Plugin</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+        
     <link rel="stylesheet" href="{{ asset('assets/vendor/medium-editor/examples/css/normalize.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/vendor/medium-editor/examples/css/demo.css') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/medium-editor@latest/dist/css/medium-editor.min.css">
